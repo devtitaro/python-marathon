1a3deaa22ce078f779554a76072d65ff41988a0b
tyroklone <tyroklonejr@gmail.com>
2021-02-18 04:53:03 -0800

    initial commit

---
+++ console_calculator.py
@@ -0,0 +1,21 @@
+# Author: Jubril Martins
+# Twitter: devtitaro_
+# Facebook: devtitaro
+# Aim: Creating a console based calculator in python using incremental development
+
+# Import the math module
+import math
+
+# Create the addition function
+def addition(param1, param2):
+    # Evaluate the result and assign it's value to a variable
+    result = param1 + param2
+
+    # Return the result
+    return result
+
+
+
+# Create the main function
+def main():
+    print('Hello World!')
